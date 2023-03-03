d7367bf1ff391a3187985385ee54be0aee23362f
shizy <shizycode@gmail.com>
2023-03-03 23:21:51 +0800

    [style] springboot jdbc - rename

--- springboot-3-user/src/main/java/com/example/DemoApplication.java
+++ springboot-3-user/src/main/java/com/example/DemoApplication.java
@@ -1,16 +1,12 @@
 package com.example;
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
